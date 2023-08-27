92d2c6075468c4f11b2381f339f13efd9849c972
tatenodev <gd28.c.tateno@gmail.com>
2023-08-27 11:03:56 +0900

    xのリンク追加

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,8 +1,21 @@
+import { tw } from "twind";
+import { css } from "twind/css";
 import { Chat } from "../islands/Chat.tsx";
+
+const RootWrap = css`
+  padding: 8px 4px;
+  margin: auto 0;
+  background: #86efac;
+  display: flex;
+  height: 100%;
+  @media screen and (max-width: 768px) {
+
+  }
+`;
 
 export default function Home() {
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac] flex h-full">
+    <div class={tw(RootWrap)}>
       <header class="max-w-screen-md flex flex-col items-center justify-center flex-1">
         <img
           class="my-2 rounded-full"
@@ -12,6 +25,29 @@ export default function Home() {
           alt="the Fresh logo: a sliced lemon dripping with juice"
         />
         <h1 class="text-4xl font-bold p-3">めめこの牢屋</h1>
+        {/* X Link */}
+        <a
+          class="flex bg-black text-white rounded"
+          style={{
+            width: 197,
+            justifyContent: "center",
+            alignItems: "center",
+            marginBottom: "10px",
+          }}
+          href="https://twitter.com/memeco2525"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            width="20"
+            height="20"
+            style={{ padding: "8px 0 6px" }}
+            src="/X-logo.svg"
+            alt="X"
+          />
+          <span style={{ paddingLeft: "10px" }}>@memeco2525</span>
+        </a>
+        {/* Twitch Link */}
         <a
           class="flex bg-[#A970FF] text-white rounded"
           style={{
@@ -33,6 +69,7 @@ export default function Home() {
           />
           <span style={{ paddingLeft: "10px" }}>Twtich</span>
         </a>
+        {/* Deno Fresh Link */}
         <a
           href="https://fresh.deno.dev"
           target="_blank"
@@ -41,7 +78,7 @@ export default function Home() {
           <img
             width="197"
             height="37"
-            src="https://fresh.deno.dev/fresh-badge-dark.svg"
+            src="https://fresh.deno.dev/fresh-badge.svg"
             alt="Made with Fresh"
           />
         </a>
