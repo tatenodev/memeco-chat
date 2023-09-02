4ff58185a87eee9efe2bc6ddee27caa9184976d4
tatenodev <gd28.c.tateno@gmail.com>
2023-09-02 21:02:33 +0900

    fix

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,7 +4,7 @@
 
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_app.tsx";
-import * as $2 from "./routes/_middleware.ts";
+import * as $2 from "./routes/api/message/_middleware.ts";
 import * as $3 from "./routes/api/message/index.ts";
 import * as $4 from "./routes/api/message/listen.ts";
 import * as $5 from "./routes/api/message/send.ts";
@@ -18,7 +18,7 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
     "./routes/_app.tsx": $1,
-    "./routes/_middleware.ts": $2,
+    "./routes/api/message/_middleware.ts": $2,
     "./routes/api/message/index.ts": $3,
     "./routes/api/message/listen.ts": $4,
     "./routes/api/message/send.ts": $5,
